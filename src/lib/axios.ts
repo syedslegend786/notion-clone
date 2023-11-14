a95ba36a3337476a4596f9f6e2fb062b5814c6cc
syedslegend786 <syedslegend786@gmail.com>
2023-11-14 11:56:57 +0500

    updated backend url in env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,3 +1,6 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/api";
+axios.defaults.baseURL =
+  process.env.NODE_ENV === "production"
+    ? process.env.BACKEND_MAIN_URL
+    : "http://localhost:3000/api";
 export { axios };
