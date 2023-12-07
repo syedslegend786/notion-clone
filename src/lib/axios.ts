5af5b06c39980d37a4f5531acd5e4e33feb7a233
syedslegend786 <syedslegend786@gmail.com>
2023-12-07 23:54:12 +0500

    updated the axios instance

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,7 @@
 import Axios from "axios";
 
-const baseURL =
-  process.env.NODE_ENV === "production"
-    ? process.env.BACKEND_MAIN_URL
-    : "http://localhost:3000/api";
 const instance = Axios.create({
-  baseURL,
+  baseURL: process.env.BACKEND_MAIN_URL,
 });
 
 export { instance as axios };
