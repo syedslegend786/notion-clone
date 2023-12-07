4d602d3e97c72e43bc9e9980f3444901484ec0dd
syedslegend786 <syedslegend786@gmail.com>
2023-12-07 23:57:31 +0500

    changes api url to next_public prefix in .env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import Axios from "axios";
 
 const instance = Axios.create({
-  baseURL: process.env.BACKEND_MAIN_URL,
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_MAIN_URL,
 });
 
 export { instance as axios };
